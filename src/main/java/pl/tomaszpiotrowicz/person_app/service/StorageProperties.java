0830a0cd1efbfdfd60d4fdb6d397e267d4a7db9d
tommu <tom.music@gazeta.pl>
2019-11-06 15:19:28 +0100

    add upload

---
+++ src/main/java/pl/tomaszpiotrowicz/person_app/service/StorageProperties.java
@@ -0,0 +1,22 @@
+package pl.tomaszpiotrowicz.person_app.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties ("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
+
